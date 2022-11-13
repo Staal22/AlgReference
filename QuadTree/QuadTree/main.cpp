d91855017d23ba4c1b37ec162a27db3361c47f2c
Mathias M <62551684+Skyress-s@users.noreply.github.com>
2022-11-13 19:04:31 +0100

    finializing things for the pres

--- QuadTree/QuadTree/main.cpp
+++ QuadTree/QuadTree/main.cpp
@@ -5,17 +5,22 @@
 
 int main() {
     QuadTree tree(Bounds(Vector2(0, 0), Vector2(100, 100)), 1);
-    tree.Insert(Vector2(0, 25));
+    std::cout << "INSERTING POINTS" <<std::endl;
     tree.Insert(Vector2(25, 25));
-    tree.Insert(Vector2(0, -25));
+    tree.Insert(Vector2(-25, 25));
+    tree.Insert(Vector2(25, -25));
     tree.Insert(Vector2(-25, -25));
 
     std::vector<Vector2> list(0);
-    tree.Query(list, Bounds(Vector2(25,25), Vector2(50,50)));
+    Bounds bounds = Bounds(Vector2(25,25), Vector2(10,10));
+    tree.Query(list, bounds);
 
+    std::cout << "|" << std::endl;
+    
+    std::cout << "Quieried points: " <<std::endl;
     for (int i = 0; i < list.size(); ++i) {
         std::cout << list[i].ToString() << std::endl;
     }
-    std::cout << tree.ToString(0);
+    // std::cout << tree.ToString(0);
     return 0;
 }
